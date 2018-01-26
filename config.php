71259e3284b906e6759011ee4f2bf82c6f4697fe
csaba <csaba.becskei@feelunique.com>
2018-01-26 08:45:48 +0000

    create config file

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+define('LOG_FILE', __DIR__.'/logs/event.log');
+define('DATABASE_HOST', 'localhost');
+define('DATABASE_NAME', 'infinity');
+define('DATABASE_USER', 'root');
+define('DATABASE_PASSWORD', 'mysql');
+define('ROOT', 'D:/wamp/www/infinity');
+define('EVENTS_UPLOAD_FOLDER', '/files/uploaded');
+define('EVENTS_PROCESSED_FOLDER', '/files/processed');
+define('EVENTS_FAILED_FOLDER', '/files/failed');
